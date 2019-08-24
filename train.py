2e148ddc42dca474539611258f49ce9e6001d695
seankelly1994 <seankearsney@gmail.com>
2019-08-24 18:47:48 +0200

    updated code

--- train.py
+++ train.py
@@ -6,6 +6,8 @@ import torch.nn.functional as F
 from torchvision import datasets, transforms, models
 import json
 
+print(torch.__version__)
+
 #Assign the data directories
 data_dir = './assets/flower_data'
 train_dir = data_dir + '/train'
@@ -48,10 +50,104 @@ dataloaders_train = torch.utils.data.DataLoader(image_datasets_train, batch_size
 with open('cat_to_name.json', 'r') as f:
     cat_to_name = json.load(f)
 
-#Use GPU if availble
-#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 #Load the pre trained network
-model = models.vgg11(pretrained=True)
+model = models.resnet50(pretrained=True)
 
+#Print out the model
+print("Model details below")
 print(model)
+
+#Turn off the gradients for the model
+for param in model.parameters():
+    param.requires_grad = False
+
+#Define the new feew forward classifier
+classifier = nn.Sequential(nn.Linear(4096, 512),
+                            nn.ReLU(),
+                            nn.Dropout(p=0.2),
+                            nn.Linear(512, 2),
+                            nn.LogSoftmax(dim=1))
+
+model.fc = classifier
+
+#Print amended model
+print("New Model details below")
+print(model.fc)
+
+#Determine the loss criterion
+criterion = nn.NLLLoss()
+
+optimizer = optim.Adam(model.fc.parameters(), lr=0.003)
+
+#####Traing the model#####
+#Setting the variables
+epochs = 1
+steps = 0
+running_loss = 0
+print_every = 5
+
+#Run through the loop for the training data
+for epoch in range(epochs):
+    for images, labels in dataloaders_train:
+        steps += 1
+
+        #images, labels = images.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+
+        log_probabilities = model(images)
+        loss = criterion(log_probabilities, labels)
+        #loss.backward()
+        optimizer.step()
+
+        #Calc running loss
+        running_loss += loss.item()
+
+        #Drop out of the trainig loop and test the accuracy on test data
+        if steps % print_every == 0:
+            model.eval()
+            test_loss = 0
+            accuracy = 0
+
+            for images, labels in dataloaders_test:
+
+                log_probabilities = model(images)
+                loss = criterion(log_probabilities, labels)
+                test_loss += loss.item()
+
+                #Calc the accuracy
+                ps = torch.exp(log_probabilities)
+                top_ps, top_class = ps.topk(1, dim=1)
+                
+                equality = top_class == labels.view(*top_class.shape)
+                accuracy += torch.mean(accuracy.type(torch.FloatTensor()))
+
+            #Print the data
+            print("Epoch data {epoch+1}/{epochs}")
+
+            running_loss = 0
+            model.train()
+
+
+
+
+#Check point data
+checkpoint = {
+    'input_size' : 4096,
+    'output_size' : 512,
+    'hidden_layers' : [each.out_features for each in model.hidden_layers],
+    'state_dict' : model.state_dict()}
+
+torch.save(checkpoint, 'checkpoint.pth')
+
+#Load the checkpoint
+def load_checkpoint(filepath):
+    checkpoint = torch.load(filepath)
+    model = model.(checkpoint['input_size'],
+                        checkpoint['output_size'],
+                        checkpoint['hidden_layers'],
+                        checkpoint['state_dict'])
+
+    model.load_state_dict(state_dict)
+
+    return model
